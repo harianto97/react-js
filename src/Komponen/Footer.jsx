67c011fa99e096e658c50ad520a39bf77c5f2805
Moh. Harianto <active.hari16@gmail.com>
2021-10-28 22:42:24 +0700

    Komponen

---
+++ src/Komponen/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return(
+            <div>
+                <hr />
+                <p>Copyrights 2021 - All Rights Reserved</p>
+            </div>
+        )
+    }
+}
+
+export default Footer;
---
+++ src/Komponen/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Header extends React.Component {
+    render(){
+        return (
+            <div>
+                <h2>Mari Belajar Web Programming</h2>
+                <hr />
+            </div>
+        )
+    }
+}
+
+export default Header;
